f9c90c7fe32fb0dc83eb02354b2cb3ff10000fdf
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-11 19:11:31 -0600

    ejemplo de funcion con parametros

--- Trabajo con funciones.py
+++ Trabajo con funciones.py
@@ -1,12 +1,10 @@
 # definición (creación) de la función
 def imprimeMensajes():
-  
-  print("Esto es un curso de python")
-  print("El curso acaba de empezar")
-  print("El curso tendrá más de 1500 vídeos")
-  print("La anterior línea era broma")
 
-# invocar = llamar = ejecutar una funcion
-imprimeMensajes()
+    return "Este es el mensaje de la función"
 
-print("El programa a terminado su ejecución")
+def imprimeMensajePersonalizado(mensaje, valor1, valor2):
+
+    return mensaje + str(valor1+valor2)
+
+print(imprimeMensajePersonalizado("La suma es ", 10, 10))
