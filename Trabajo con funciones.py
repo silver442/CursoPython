08c254c5e7b25ffe3b21c1dbb1a8a00d058b60b4
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-11 18:46:02 -0600

    Creacion de la primer función

---
+++ Trabajo con funciones.py
@@ -0,0 +1,12 @@
+# definición (creación) de la función
+def imprimeMensajes():
+  
+  print("Esto es un curso de python")
+  print("El curso acaba de empezar")
+  print("El curso tendrá más de 1500 vídeos")
+  print("La anterior línea era broma")
+
+# invocar = llamar = ejecutar una funcion
+imprimeMensajes()
+
+print("El programa a terminado su ejecución")
