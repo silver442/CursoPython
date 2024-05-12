19a37c5c2c25b6e5221d57b64ee83af9483c426e
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-11 19:31:30 -0600

    Uso de las converiones int, str, float, split, y join

---
+++ Trabajo con conversiones.py
@@ -0,0 +1,15 @@
+a="15.25"
+
+b="35.35"
+
+print(float(a)+float(b))
+
+# Uso de join .Se necesita un separador antes del join
+empleados=["Ana","Oscar","Maria","Pedro","Juan"]
+
+print(". ".join(empleados))
+
+# convertir un String en una lista
+empleados2="Ana, Oscar, Maria, Pedro, Juan"
+
+print(empleados2.split())
