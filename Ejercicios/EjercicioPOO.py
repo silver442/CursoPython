4f4cc81704b2fc42f08cdcfa7e2a91cf54a1366d
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-17 17:05:49 -0600

    ejercicio de herencia

--- Ejercicios/EjercicioPOO.py
+++ Ejercicios/EjercicioPOO.py
@@ -1,7 +1,4 @@
 class CuentaCorriente():
-    nCuenta=""
-    titular=""
-    saldo=0
 
     def __init__(self, nCuenta, titular, saldo):
         self.nCuenta=nCuenta
@@ -18,9 +15,22 @@ class CuentaCorriente():
     def retirarDinero(self, dineroRetirar):
 
         self.saldo-=dineroRetirar
+
+class CuentaJoven(CuentaCorriente):
+
+    def __init__(self, nCuenta, titular, saldo, bonus_promocion=0):
+        super().__init__(nCuenta,titular, saldo)
+        self.bonus_promocion=bonus_promocion
+        saldo+=bonus_promocion
+
+    def getBonus(self):
+        return self.bonus_promocion
     
+    def infCuenta(self):
+        return super().infCuenta() + " Bonu: " + str(self.bonus_promocion)
+
 cuenta1=CuentaCorriente("001","Silver",2500)
-cuenta2=CuentaCorriente("001","Silver",2500)
+cuenta2=CuentaJoven("002","Silver2",2500)
 
 print(cuenta1.infCuenta())
 
@@ -28,3 +38,6 @@ cuenta1.ingresarDinero(500)
 cuenta1.retirarDinero(100)
 
 print(cuenta1.infCuenta())
+
+cuenta2.ingresarDinero(200)
+print(cuenta2.infCuenta())
