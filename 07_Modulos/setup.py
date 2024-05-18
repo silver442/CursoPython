05a7ce4610bcbf075875007b3af3132b931ba515
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-18 14:59:07 -0600

    creacion de paquete distribuible

---
+++ 07_Modulos/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+    name="calculosbasicosmatematicos",
+    version="1.0",
+    description="Paquete para cálculos básicos, suma, resta, multiplicación",
+    author="Silvestre",
+    author_email="silvestre.hdz.h@gmail.com",
+    url="www.silvestrehdz.com",
+    packages=["moduloMatematico", "moduloMatematico.calculosBasicos"]
+
+)
--- 07_Modulos/tirar/Trabajo con modulos.py
+++ 07_Modulos/tirar/Trabajo con modulos.py
@@ -3,7 +3,7 @@
 # Para no utilizar el nombre del modulo adelante
 # subpaquetes en paquetes
 from moduloMatematico.calculosBasicos.funciones_matematicas import * # * para importar todo
-from moduloMatematico.otrosCalculos.PotenciaYRedondeo import * # * para importar todo
+# from moduloMatematico.otrosCalculos.PotenciaYRedondeo import * # * para importar todo
 
 sumar(8,2)
 
@@ -11,6 +11,6 @@ restar(15,8)
 
 multiplicar(9,8)
 
-potencia(5,3)
+# potencia(5,3)
 
-redondear(2.78)
+# redondear(2.78)
