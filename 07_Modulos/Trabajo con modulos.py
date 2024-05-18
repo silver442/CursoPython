d58976c935bde4306a307d8c1a5414099baf36e9
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-18 14:07:19 -0600

    uso de modulos

---
+++ 07_Modulos/Trabajo con modulos.py
@@ -0,0 +1,10 @@
+# import funciones_matematicas
+
+# Para no utilizar el nombre del modulo adelante
+from moduloMatematico.funciones_matematicas import * # * para importar todo
+
+sumar(8,2)
+
+restar(15,8)
+
+multiplicar(9,8)
---
+++ 07_Modulos/moduloMatematico/funciones_matematicas.py
@@ -0,0 +1,11 @@
+def sumar(num1, num2):
+
+    print("EL resultado de la suma es: ", num1+num2)
+
+def restar(num1, num2):
+
+    print("EL resultado de la resta es: ", num1-num2)
+
+def multiplicar(num1, num2):
+
+    print("EL resultado de la multiplicación es: ", num1*num2)
