6875e6ff66bc4be7165788ee340c2dba88355762
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-18 14:25:20 -0600

    Trabajanco con paquetes y subpaquetes

--- 07_Modulos/Trabajo con modulos.py
+++ 07_Modulos/Trabajo con modulos.py
@@ -1,10 +1,16 @@
 # import funciones_matematicas
 
 # Para no utilizar el nombre del modulo adelante
-from moduloMatematico.funciones_matematicas import * # * para importar todo
+# subpaquetes en paquetes
+from moduloMatematico.calculosBasicos.funciones_matematicas import * # * para importar todo
+from moduloMatematico.otrosCalculos.PotenciaYRedondeo import * # * para importar todo
 
 sumar(8,2)
 
 restar(15,8)
 
 multiplicar(9,8)
+
+potencia(5,3)
+
+redondear(2.78)
---
+++ 07_Modulos/moduloMatematico/otrosCalculos/PotenciaYRedondeo.py
@@ -0,0 +1,7 @@
+def potencia(base, exponente):
+
+    print("El resultado de la potencia es: ", base**exponente)
+
+def redondear(numero):
+
+    print("El número redondeaado es: ", round(numero))
