3c8b74c9c32c920d4f07185b0f4629d454e1ccd6
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-05-21 13:45:54 -0600

    profundizando en los vinculos

--- 08_Crawlers/Trabajo con crawlers.py
+++ 08_Crawlers/Trabajo con crawlers.py
@@ -4,6 +4,8 @@ import requests
 
 from urllib.parse import urljoin
 
+import time
+
 class PostCrawled():
 
     def __init__(self, titulo, emoticono, contenido, imagen):
@@ -19,21 +21,34 @@ class PostExtractor():
 
         urlBase="https://python.beispiel.programmierenlernen.io/"
 
-        miDoc=requests.get(urlBase)
-
-        # extraer el documento
-        docFinal=BeautifulSoup(miDoc.text, "html.parser")
-
         posts=[]
 
-        for card in docFinal.select(".card"):
-            titulo=card.select(".card-title span")[1].text
-            emoticono=card.select_one(".emoji").text
-            contenido=card.select_one(".card-text").text
-            imagen=urljoin(urlBase,card.select_one("img").attrs["src"])
+        while urlBase!="":
 
-            crawled=PostCrawled(titulo,emoticono, contenido, imagen)
-            posts.append(crawled)
+            miDoc=requests.get(urlBase)
+
+            # extraer el documento
+            docFinal=BeautifulSoup(miDoc.text, "html.parser")
+
+            time.sleep(2) # pausa de 2 segundos
+
+            for card in docFinal.select(".card"):
+                titulo=card.select(".card-title span")[1].text
+                emoticono=card.select_one(".emoji").text
+                contenido=card.select_one(".card-text").text
+                imagen=urljoin(urlBase,card.select_one("img").attrs["src"])
+
+                crawled=PostCrawled(titulo,emoticono, contenido, imagen)
+                posts.append(crawled)
+
+            boton_siguiente=docFinal.select_one(".navigation .btn")
+
+            if boton_siguiente:
+                rutas_absolutas=urljoin(urlBase,boton_siguiente.attrs["href"])
+                urlBase=rutas_absolutas
+                print(rutas_absolutas)
+            else:
+                urlBase=""
 
         return posts
 
