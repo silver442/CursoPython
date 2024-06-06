1c6c5ff4ed5dfab994f12657f72bcd79fbf6f046
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-06-06 15:00:20 -0600

    Creacion de base de datos con una table con ID unico

---
+++ 15_TrabajoBBDD/PracticaClave.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+miConexion=sqlite3.connect("15_TrabajoBBDD/GestionPedidos")
+
+miCursor=miConexion.cursor()
+
+miCursor.execute('''
+   CREATE TABLE PRODUCTOS(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE_ARTICULO VARCHAR(40),
+        PRECIO INTEGER,
+        SECCION VARCHAR(20)
+   )                           
+''')
+
+misProductos=[
+    ("pelota", 15, "deportes"),
+    ("cenicero", 25, "ceramica"),
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES(NULL, ?, ?, ?)", misProductos)
+
+miConexion.commit()
+
+miCursor.close()
+
+miConexion.close()
