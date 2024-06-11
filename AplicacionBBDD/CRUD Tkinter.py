842d6683742d09f5cb7d36cd5b43e560aad34a1c
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-06-11 12:42:36 -0600

    insertando campos de textos

--- AplicacionBBDD/CRUD Tkinter.py
+++ AplicacionBBDD/CRUD Tkinter.py
@@ -29,9 +29,34 @@ barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
 barraMenu.add_cascade(label="CRUD", menu=crudMenu)
 barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
 
+
 #-------------------------construccion de campos grid-------------------
 
 miFrame=Frame(root)
 miFrame.pack()
+
+cuadroId=Entry(miFrame)
+cuadroId.grid(row=0,column=1, padx=10, pady=10)
+
+cuadroNombre=Entry(miFrame)
+cuadroNombre.grid(row=1,column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red", justify="right")
+
+cuadroPass=Entry(miFrame)
+cuadroPass.grid(row=2,column=1, padx=10, pady=10)
+cuadroPass.config(show="?")
+
+cuadroApellido=Entry(miFrame)
+cuadroApellido.grid(row=3,column=1, padx=10, pady=10)
+
+cuadroDireccion=Entry(miFrame)
+cuadroDireccion.grid(row=4,column=1, padx=10, pady=10)
+
+textoComentario=Text(miFrame, width=16, height=5)
+textoComentario.grid(row=5, column=1, padx=10, pady=10)
+scrollVert=Scrollbar(miFrame, command=textoComentario.yview)
+scrollVert.grid(row=5, column=2, sticky="nsew")
+textoComentario.config(yscrollcommand=scrollVert.set)
+
 
 root.mainloop()
