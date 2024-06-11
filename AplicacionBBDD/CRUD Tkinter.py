a15745d4ada1a040c4f9883b52d2575a44273583
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-06-11 12:23:01 -0600

    practica guiada, construccion del menu de la aplicacion

---
+++ AplicacionBBDD/CRUD Tkinter.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+root=Tk()
+
+barraMenu=Menu(root)
+root.config(menu=barraMenu, width=300, height=300)
+
+bbddMenu=Menu(barraMenu, tearoff=0)
+bbddMenu.add_command(label="Conectar")
+bbddMenu.add_command(label="Salir")
+
+borrarMenu=Menu(barraMenu, tearoff=0)
+borrarMenu.add_command(label="Borrar")
+
+crudMenu=Menu(barraMenu, tearoff=0)
+crudMenu.add_command(label="Crear")
+crudMenu.add_command(label="Leer")
+crudMenu.add_command(label="Actualizar")
+crudMenu.add_command(label="Borrar")
+
+ayudaMenu=Menu(barraMenu, tearoff=0)
+ayudaMenu.add_command(label="Licencia")
+ayudaMenu.add_command(label="Acerca de...")
+
+barraMenu.add_cascade(label="BBDD", menu=bbddMenu)
+barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
+barraMenu.add_cascade(label="CRUD", menu=crudMenu)
+barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
+
+#-------------------------construccion de campos grid-------------------
+
+miFrame=Frame(root)
+miFrame.pack()
+
+root.mainloop()
