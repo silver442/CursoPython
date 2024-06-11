a73622d7b2d0c43f4ea41de7f6ed614490b9ebd1
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-06-11 12:53:36 -0600

    creacion de label y botones de la parte inferior de la aplicacion

--- AplicacionBBDD/CRUD Tkinter.py
+++ AplicacionBBDD/CRUD Tkinter.py
@@ -58,5 +58,42 @@ scrollVert=Scrollbar(miFrame, command=textoComentario.yview)
 scrollVert.grid(row=5, column=2, sticky="nsew")
 textoComentario.config(yscrollcommand=scrollVert.set)
 
+# ----------creación de Labels-------------------
+
+idLabel=Label(miFrame, text="Id:")
+idLabel.grid(row=0, column=0, sticky="e" , padx=10, pady=10)
+
+nombreLabel=Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=1, column=0, sticky="e" , padx=10, pady=10)
+
+passLabel=Label(miFrame, text="Password:")
+passLabel.grid(row=2, column=0, sticky="e" , padx=10, pady=10)
+
+apellidoLabel=Label(miFrame, text="Apellido:")
+apellidoLabel.grid(row=3, column=0, sticky="e" , padx=10, pady=10)
+
+direccionLabel=Label(miFrame, text="Dirección:")
+direccionLabel.grid(row=4, column=0, sticky="e" , padx=10, pady=10)
+
+comentariosLabel=Label(miFrame, text="Comentarios:")
+comentariosLabel.grid(row=5, column=0, sticky="e" , padx=10, pady=10)
+
+#--------------colocación de botones------------------------
+
+miFrameBotones=Frame(root)
+miFrameBotones.pack()
+
+botonCrear=Button(miFrameBotones, text="Crear")
+botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+botonLeer=Button(miFrameBotones, text="Leer")
+botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
+
+botonActualizar=Button(miFrameBotones, text="Actualizar")
+botonActualizar.grid(row=1, column=2, sticky="e", padx=10, pady=10)
+
+botonBorrar=Button(miFrameBotones, text="Borrar")
+botonBorrar.grid(row=1, column=3, sticky="e", padx=10, pady=10)
+
 
 root.mainloop()
